ec8226b757d7f73263b7d81f3805c83793e8fd22
brunolaerte <bruno_laerte@hotmail.com>
2021-08-25 19:00:29 -0300

    primeiros entendimentos ok, vamos para as procimas aulas

---
+++ Modulo - Fundamentos do Desenvolvimento Web/BLOCO-04/bloco-4.1/primeirosPassosJavaScript.js
@@ -0,0 +1,10 @@
+const myName = "Bruno";
+const birthCity = "Jacobina-BA";
+let birthYear = 1992;
+
+console.log(myName, birthCity, birthYear);
+
+//  o JS tipa dinamicamentoe as variaveis e os tipos principais são 
+// string, bolean, number, undefined
+// quando declaramos uma variavel e não atribuimos valor a ela, o javascript interpreta que esta variavel é do tipo undefined
+// se tivermos uma variavel numero que esta entre aspas o JS interpretara que ela é uma string por conta das aspas. 
