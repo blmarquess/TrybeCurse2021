507301cf7631439a7aef10bb5a2e96aabd59aa2f
brunolaerte <bruno_laerte@hotmail.com>
2021-08-27 18:30:32 -0300

    projeto terminado vamos para o JS

---
+++ Modulo - Fundamentos do Desenvolvimento Web/BLOCO-04/bloco-4.1/javascript4.2.js
@@ -0,0 +1,31 @@
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//  console.log(numero);
+//  }
+// resultado: 
+//1
+//2
+//3
+//4
+//5
+
+let word = 'HelloWorld';
+for (let variavel of word) {
+  console.log(variavel);
+}
+// resultado:
+//[Done] exited with code=0 in 0.069 seconds
+//
+//[Running] node "/home/bruno/trybe-curse/git_course/TrybeCurse2021/Modulo - Fundamentos do Desenvolvimento Web/BLOCO-04/bloco-4.1/javascript4.2.js"
+//H
+//e
+//l
+//l
+//o
+//W
+//o
+//r
+//l
+//d
+
+// o for/of não consegue alterar os valored dos arrays so o s coletaros para exibir 
--- Modulo - Fundamentos do Desenvolvimento Web/BLOCO-04/bloco-4.1/primeirosPassosJavaScript.js
+++ Modulo - Fundamentos do Desenvolvimento Web/BLOCO-04/bloco-4.1/primeirosPassosJavaScript.js
@@ -8,3 +8,15 @@ console.log(myName, birthCity, birthYear);
 // string, bolean, number, undefined
 // quando declaramos uma variavel e não atribuimos valor a ela, o javascript interpreta que esta variavel é do tipo undefined
 // se tivermos uma variavel numero que esta entre aspas o JS interpretara que ela é uma string por conta das aspas. 
+
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
+
+// se uma das constantes não fossem cafézinho  pão na chapa, o JavaScript retornaria como fale e acionaria o resultado do else
+
