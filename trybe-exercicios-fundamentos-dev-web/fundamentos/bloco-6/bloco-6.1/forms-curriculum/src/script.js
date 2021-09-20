a70dcf3213189dcb31c6098274a4d220f91535c5
blmarquess <blmarques.dev@gmail.com>
2021-09-20 19:01:25 -0300

    add exercicios dia 6.1 6.2 e 6.3

--- trybe-exercicios-fundamentos-dev-web/fundamentos/bloco-6/bloco-6.1/forms-curriculum/src/script.js
+++ trybe-exercicios-fundamentos-dev-web/fundamentos/bloco-6/bloco-6.1/forms-curriculum/src/script.js
@@ -12,8 +12,6 @@ addUFs();
 
 function validaData() {
   const dateInsp = document.querySelector('#dataInicio');
-  if (dateInsp !== RegExp((0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d{2}) ) {
-    return alert("");
-  }
+  return dateInsp;
 }
 validaData();
