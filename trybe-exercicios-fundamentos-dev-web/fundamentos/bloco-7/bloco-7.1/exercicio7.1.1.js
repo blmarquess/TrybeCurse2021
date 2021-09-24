2dc510ee2651c61ee13a1dc619a6cb491276eadc
bruno Marques <blmarques.dev@gmail.com>
2021-09-23 22:54:29 -0300

    exercicios 7.1

---
+++ trybe-exercicios-fundamentos-dev-web/fundamentos/bloco-7/bloco-7.1/exercicio7.1.1.js
@@ -0,0 +1,41 @@
+// exec001
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+// Copie o código abaixo.
+
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+// Minha resposta 
+const test = (escopo) => {
+  escopo === true ? console.log('Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !') :  console.log(`Não devo ser utilizada fora meu escopo (else)`);
+}
+test(true);
+
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// // Seu código aqui.
+
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const slice = array => array.sort() > array. array.slice(',');
+
+console.log(slice(oddsAndEvens)); // será necessário alterar essa linha 😉
