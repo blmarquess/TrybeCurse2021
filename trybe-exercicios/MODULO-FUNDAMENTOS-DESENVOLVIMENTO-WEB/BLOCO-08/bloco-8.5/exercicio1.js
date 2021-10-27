647cd2fecba26115b87f96a1e08dee485bed4e20
Bruno <blmarques.dev@gmail.com>
2021-10-27 16:44:09 -0300

    Start Modulu Front End

---
+++ trybe-exercicios/MODULO-FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO-08/bloco-8.5/exercicio1.js
@@ -0,0 +1,9 @@
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => console.log(rectangle[0] * rectangle[1]));
+
--- trybe-exercicios/MODULO-FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO-08/bloco-8.5/spred.js
+++ trybe-exercicios/MODULO-FUNDAMENTOS-DESENVOLVIMENTO-WEB/BLOCO-08/bloco-8.5/spred.js
@@ -1,4 +1,58 @@
+// const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+// const patientInfo = [60, 1.7];
+
+// console.log(imc(...patientInfo)); // 20.76
+
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months); /* [
+  'JAN', 'FEV', 'MAR',
+  'ABR', 'MAI', 'JUN',
+  'JUL', 'AGO', 'SET',
+  'OUT', 'NOV', 'DEZ'
+] */
 const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
 const patientInfo = [60, 1.7];
 
 console.log(imc(...patientInfo)); // 20.76
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+console.log(Math.max(...randomNumbers)); // 800
+console.log(Math.min(...randomNumbers)); // 5
+
+const randomNumbers2 = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+console.log(Math.max(...randomNumbers2)); // 800
+console.log(Math.min(...randomNumbers2)); // 5
+const people = {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+};
+
+const about = {
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer',
+};
+
+const customer = { ...people, ...about };
+console.log(customer);
+// { id: 101,
+//   name: 'Alysson',
+//   age: 25,
+//   address: 'Av. Getúlio Vargas, 1000',
+//   occupation: 'Developer'}
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['manga', 'ova', 'limao'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['suco', 'sorvete', 'sandwish'];
+
+const fruitSalad = (fruit, additional) => fSalad = [...fruit, ...additional];
+
+console.log(fruitSalad(specialFruit, additionalItens));
