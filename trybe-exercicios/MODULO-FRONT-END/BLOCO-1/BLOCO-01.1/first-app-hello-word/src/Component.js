69f8a41ac53d9d7f479f5b44591f2ee7987abff5
Bruno <blmarques.dev@gmail.com>
2021-10-28 00:28:11 -0300

    Intall and ron first react app

---
+++ trybe-exercicios/MODULO-FRONT-END/BLOCO-1/BLOCO-01.1/first-app-hello-word/src/Component.js
@@ -0,0 +1,11 @@
+const nome = 'Bruno';
+const sobName = 'Marques';
+const apendendo = 'DevOps';
+
+function AboutMe() {
+
+  return <h1> Meu nome é: {nome} {sobName} e estou estudando na Trybe para me tornar um {apendendo}!</h1>;
+
+};
+
+export default AboutMe;
