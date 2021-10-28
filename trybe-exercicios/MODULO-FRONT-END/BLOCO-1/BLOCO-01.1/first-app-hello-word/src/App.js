1134d55d4f57719eaf8e74c9411c4a7922d5783f
Bruno <blmarques.dev@gmail.com>
2021-10-28 01:05:19 -0300

    First App - Exercicio-01

--- trybe-exercicios/MODULO-FRONT-END/BLOCO-1/BLOCO-01.1/first-app-hello-word/src/App.js
+++ trybe-exercicios/MODULO-FRONT-END/BLOCO-1/BLOCO-01.1/first-app-hello-word/src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import React from 'react';
 import AboutMe from './Component';
 
+const Compromissos = ['estuda', 'anotar', 'praticar', 'revisar'];
+
+
+const Task = () => {
+  return (Compromissos.map(file => <li key={file}>{file}</li>)
+    
+  );
+}
+
+
+
 function App() {
   return (
 
-    AboutMe()
+    // AboutMe();
 
+  Task()
   );
 }
 
